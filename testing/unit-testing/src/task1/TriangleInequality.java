1569f93cc908bde86eadfee34ff3a3f287a945d6
RomanBorr <starmordar@gmail.com>
2019-10-21 23:15:13 +0300

    add assertExceptions to unit-test

--- testing/unit-testing/src/task1/TriangleInequality.java
+++ testing/unit-testing/src/task1/TriangleInequality.java
@@ -1,11 +1,16 @@
 package task1;
 
 public class TriangleInequality {
+    private static final String ERROR_LENGTH_SIDE = "Triangle side can't be less then 0";
+    private static final String ERROR_MAX_LENGTH_SIDE = "Triangle side can't be max double value";
 
     public static boolean isTriangleExists(double a, double b, double c) {
-        if (a <= 0 || b <= 0 || c <= 0) return false;
-        if ((a + b) > c && (a + c) > b && (b + c) > a) return true;
-        return false;
+        if (a <= 0 || b <= 0 || c <= 0) throw new IllegalArgumentException(ERROR_LENGTH_SIDE);
+
+        if (a == Double.MAX_VALUE || b == Double.MAX_VALUE || c == Double.MAX_VALUE)
+            throw new IllegalArgumentException(ERROR_MAX_LENGTH_SIDE);
+
+        return (a + b) > c && (a + c) > b && (b + c) > a;
     }
 
 }
--- testing/unit-testing/src/task1/TriangleInequalityTest.java
+++ testing/unit-testing/src/task1/TriangleInequalityTest.java
@@ -2,62 +2,72 @@ package task1;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TriangleInequalityTest {
 
     @Test
     public void equilateralTriangle() {
-        assertEquals(true, TriangleInequality.isTriangleExists(12.7, 12.7, 12.7));
+        assertTrue(TriangleInequality.isTriangleExists(12.7, 12.7, 12.7));
     }
 
     @Test
     public void allZeroSides() {
-        assertEquals(false, TriangleInequality.isTriangleExists(0.0, 0.0, 0.0));
+        assertThrows(IllegalArgumentException.class, () -> {
+            TriangleInequality.isTriangleExists(0.0, 0.0, 0.0);
+        });
     }
 
     @Test
     public void oneZeroSide() {
-        assertEquals(false, TriangleInequality.isTriangleExists(0, 12.0, 34.0));
+        assertThrows(IllegalArgumentException.class, () -> {
+            TriangleInequality.isTriangleExists(0, 12.0, 34.0);
+        });
     }
 
     @Test
     public void oneNegativeSide() {
-        assertEquals(false, TriangleInequality.isTriangleExists(17.0, -23.1, 34.0));
+        assertThrows(IllegalArgumentException.class, () -> {
+            TriangleInequality.isTriangleExists(17.0, -23.1, 34.0);
+        });
     }
 
     @Test
     public void realScaleneTriangle() {
-        assertEquals(true, TriangleInequality.isTriangleExists(12.2, 7.4, 14.5));
+        assertTrue(TriangleInequality.isTriangleExists(12.2, 7.4, 14.5));
     }
 
     @Test
     public void realIsoscelesTriangle() {
-        assertEquals(true, TriangleInequality.isTriangleExists(12.5, 12.5, 14.2));
+        assertTrue(TriangleInequality.isTriangleExists(12.5, 12.5, 14.2));
     }
 
     @Test
     public void sumOfTwoSidesEqualsThird() {
-        assertEquals(false, TriangleInequality.isTriangleExists(12.5, 12.5, 25.0));
+        assertFalse(TriangleInequality.isTriangleExists(12.5, 12.5, 25.0));
     }
 
     @Test
     public void sumOfTwoSidesGreaterThird() {
-        assertEquals(false, TriangleInequality.isTriangleExists(12.6, 99.3, 24.7));
+        assertFalse(TriangleInequality.isTriangleExists(12.6, 99.3, 24.7));
     }
 
     @Test
     public void sumOfTwoSidesLessThird() {
-        assertEquals(false, TriangleInequality.isTriangleExists(12.1, 11.5, 24.9));
+        assertFalse(TriangleInequality.isTriangleExists(12.1, 11.5, 24.9));
     }
 
     @Test
     public void oneOfTheSideIsMaxDouble() {
-        assertEquals(false, TriangleInequality.isTriangleExists(Double.MAX_VALUE , 5.0, 12.0));
+        assertThrows(IllegalArgumentException.class, () -> {
+            TriangleInequality.isTriangleExists(Double.MAX_VALUE, 5.0, 12.0);
+        });
     }
 
     @Test
     public void allSidesIsMaxDouble() {
-        assertEquals(true, TriangleInequality.isTriangleExists(Double.MAX_VALUE , Double.MAX_VALUE, Double.MAX_VALUE));
+        assertThrows(IllegalArgumentException.class, () -> {
+            TriangleInequality.isTriangleExists(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+        });
     }
 }
